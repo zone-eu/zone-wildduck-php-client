34ee353dbd01e1cbc688dd22fa63c78f7d39b63d
Karl Viiburg <karl.viiburg@twn.ee>
2018-06-19 09:12:38 +0300

    Updated facade

--- src/Facades/Wildduck.php
+++ src/Facades/Wildduck.php
@@ -3,12 +3,25 @@
 namespace Wildduck\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Wildduck\Api\Addresses;
+use Wildduck\Api\ApplicationPasswords;
+use Wildduck\Api\Authentication;
+use Wildduck\Api\Autoreplies;
+use Wildduck\Api\Filters;
+use Wildduck\Api\TwoFactorAuth;
+use Wildduck\Api\Users;
 use Wildduck\Client;
 
 class Wildduck extends Facade
 {
     /**
-     * @return Client
+     * @method static Addresses address()
+     * @method static ApplicationPasswords applicationPasswords()
+     * @method static Authentication authentication()
+     * @method static Autoreplies autoreplies()
+     * @method static Filters filters()
+     * @method static TwoFactorAuth twoFactorAuth()
+     * @method static Users users()
      */
     protected static function getFacadeAccessor()
     {
