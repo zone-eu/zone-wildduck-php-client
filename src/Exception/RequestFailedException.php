bb3a11b6afc71e509f1f30ea8c78da8901c857e1
Karl Viiburg <karl.viiburg@twn.ee>
2020-07-01 14:54:05 +0300

    fix(Exception) - proper argument handling on request failure

--- src/Exception/RequestFailedException.php
+++ src/Exception/RequestFailedException.php
@@ -5,8 +5,16 @@ namespace Zone\Wildduck\Exception;
 class RequestFailedException extends WildduckException
 {
 
+    private ?string $errorCode;
+
     public function __construct(string $message, string $code = 'error.request')
     {
-        parent::__construct($message, $code);
+        $this->errorCode = $code;
+        parent::__construct($message, 0);
+    }
+
+    public function getErrorCode()
+    {
+        return $this->errorCode;
     }
 }
