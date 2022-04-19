069bb7fa18993e7c81c1539bf4012cc7c45ce5d0
Joosep Jõeleht <Joosep.Joeleht@twn.ee>
2022-04-19 11:32:05 +0300

    release(1.3.0) - Implemented new WebAuthN endpoints to TwoFactorAuthenticationService.php
    Also removed old U2F endpoints support.

--- src/Service/TwoFactorAuthenticationService.php
+++ src/Service/TwoFactorAuthenticationService.php
@@ -2,66 +2,181 @@
 
 namespace Zone\Wildduck\Service;
 
+use Zone\Wildduck\Exception\ApiConnectionException;
+use Zone\Wildduck\Exception\AuthenticationFailedException;
+use Zone\Wildduck\Exception\InvalidAccessTokenException;
+use Zone\Wildduck\Exception\RequestFailedException;
+use Zone\Wildduck\Exception\ValidationException;
+use Zone\Wildduck\WildduckObject;
+
 class TwoFactorAuthenticationService extends AbstractService
 {
 
-    public function disable(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function disable(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('delete', $this->buildPath('/users/%s/2fa', $user), $params, $opts);
     }
 
-    public function disableTOTPAuth(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function disableTOTPAuth(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('delete', $this->buildPath('/users/%s/2fa/totp', $user), $params, $opts);
     }
 
-    public function disableCustom(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function disableCustom(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('delete', $this->buildPath('/users/%s/2fa/custom', $user), $params, $opts);
     }
 
-    public function enableTOTPSeed(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function enableTOTPSeed(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('post', $this->buildPath('/users/%s/2fa/totp/enable', $user), $params, $opts);
     }
 
-    public function enableCustom(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function enableCustom(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('put', $this->buildPath('/users/%s/2fa/custom', $user), $params, $opts);
     }
 
-    public function generateTOTPSeed(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function generateTOTPSeed(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('post', $this->buildPath('/users/%s/2fa/totp/setup', $user), $params, $opts);
     }
 
-    public function validateTOTPToken(string $user, $params = null, $opts = null)
+    /**
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function validateTOTPToken(string $user, $params = null, $opts = null): WildduckObject
     {
         return $this->request('post', $this->buildPath('/users/%s/2fa/totp/check', $user), $params, $opts);
     }
 
-    public function generateU2F(string $user, $params = null, $opts = null)
+    /**
+     * Gets the challenge used to register a new WebAuthN key
+     *
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function webAuthNCredentials(string $user, $params = null, $opts = null): WildduckObject
     {
-        return $this->request('post', $this->buildPath('/users/%s/2fa/u2f/setup', $user), $params, $opts);
+        return $this->requestCollection('get', $this->buildPath('/users/%s/2fa/webauthn/credentials', $user), $params, $opts);
     }
 
-    public function enableU2F(string $user, $params = null, $opts = null)
+    /**
+     * Gets the challenge used to register a new WebAuthN key
+     *
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function webAuthNRegistrationChallenge(string $user, $params = null, $opts = null): WildduckObject
     {
-        return $this->request('post', $this->buildPath('/users/%s/2fa/u2f/enable', $user), $params, $opts);
+        return $this->request('post', $this->buildPath('/users/%s/2fa/webauthn/registration-challenge', $user), $params, $opts);
     }
 
-    public function disableU2F(string $user, $params = null, $opts = null)
+    /**
+     * Attests the credential used to register a new WebAuthN key
+     *
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function webAuthNRegistrationAttestation(string $user, $params = null, $opts = null): WildduckObject
     {
-        return $this->request('delete', $this->buildPath('/users/%s/2fa/u2f', $user), $params, $opts);
+        return $this->request('post', $this->buildPath('/users/%s/2fa/webauthn/registration-attestation', $user), $params, $opts);
     }
 
-    public function startU2F(string $user, $params = null, $opts = null)
+    /**
+     * Removes the credential for the user
+     *
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function webAuthNRemoveCredential(string $user, string $credentialId, $params = null, $opts = null): WildduckObject
     {
-        return $this->request('post', $this->buildPath('/users/%s/2fa/u2f/start', $user), $params, $opts);
+        return $this->request('delete', $this->buildPath('/users/%s/2fa/webauthn/credentials/%s', $user, $credentialId), $params, $opts);
     }
 
-    public function validateU2F(string $user, $params = null, $opts = null)
+    /**
+     * Gets the challenge used for authentication with a WebAuthN compatible key
+     *
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function webAuthNAuthenticationChallenge(string $user, $params = null, $opts = null): WildduckObject
     {
-        return $this->request('post', $this->buildPath('/users/%s/2fa/u2f/check', $user), $params, $opts);
+        return $this->request('post', $this->buildPath('/users/%s/2fa/webauthn/authentication-challenge', $user), $params, $opts);
+    }
+
+    /**
+     * Asserts that the credential returned from the WebAuthN compatible key is allowed for the user
+     *
+     * @throws RequestFailedException
+     * @throws InvalidAccessTokenException
+     * @throws AuthenticationFailedException
+     * @throws ApiConnectionException
+     * @throws ValidationException
+     */
+    public function webAuthNAuthenticationAssertion(string $user, $params = null, $opts = null): WildduckObject
+    {
+        return $this->request('post', $this->buildPath('/users/%s/2fa/webauthn/authentication-assertion', $user), $params, $opts);
     }
 }
