460d6782ce9a48242ab6ab53e1c4538ef0420387
jissepo <jissepo@users.noreply.github.com>
2025-06-11 15:00:40 +0300

    fixed incorrect variable name caused by merge

--- src/ApiRequestor.php
+++ src/ApiRequestor.php
@@ -56,7 +56,7 @@ class ApiRequestor
      * @param null|string $accessToken
      * @param null|string $apiBase
      */
-    public function __construct(?string $apiKey = null, ?string $apiBase = null)
+    public function __construct(?string $accessToken = null, ?string $apiBase = null)
     {
         $this->_accessToken = $accessToken;
         if (!$apiBase) {
