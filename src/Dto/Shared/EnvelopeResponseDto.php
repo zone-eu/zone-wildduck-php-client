0c60cb3eeb9911697cad2de4f93c15ffcf3c5020
Joosep Jõeleht <joosep@zone.ee>
2026-01-16 16:25:11 +0200

    Fixed envelope typing

--- src/Dto/Shared/EnvelopeResponseDto.php
+++ src/Dto/Shared/EnvelopeResponseDto.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Zone\Wildduck\Dto\Shared;
 
 use Zone\Wildduck\Dto\ResponseDtoInterface;
+use Zone\Wildduck\Exception\DtoValidationException;
 
 /**
  * SMTP envelope information
@@ -12,16 +13,18 @@ use Zone\Wildduck\Dto\ResponseDtoInterface;
 readonly class EnvelopeResponseDto implements ResponseDtoInterface
 {
     public function __construct(
-        public ?string $from = null,
+        public string $from,
         /** @var string[]|null */
         public ?array $rcpt = null,
-    ) {
-    }
+    ) {}
 
     public static function fromArray(array $data): self
     {
+        if (!isset($data['from'])) {
+            throw DtoValidationException::missingRequiredField('from', 'string');
+        }
         return new self(
-            from: $data['from'] ?? null,
+            from: $data['from'],
             rcpt: $data['rcpt'] ?? null,
         );
     }
